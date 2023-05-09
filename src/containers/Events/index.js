a1749805254a9ba25becd47ed78674cb12208a8f
Eddy <eddytougon@gmail.com>
2023-05-09 17:46:24 +0100

    fix: correction bug filtre réalisation

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -45,7 +45,6 @@ const EventList = () => {
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-        
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
